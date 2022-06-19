748cb74c6c2031b614bd6b360d35c5831517b5de
Jon Denman <denmanjonathan@gmail.com>
2022-06-19 13:11:25 -0700

    refactor book constructor as class

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-
-    this.info = function() {
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    
+    info() {
         readString = this.read ? 'read' : 'not read yet';
         return `${this.title} by ${this.author}, ${pages} pages, ${readString}`;
     }
